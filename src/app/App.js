a7c9fed9dbd1aa8dc69b9f1dedaeba112bcee60b
saramburo <106128587+saramburo@users.noreply.github.com>
2024-01-25 15:02:21 -0500

    upload files

---
+++ src/app/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import About from "../components/About";
+import SignUp from "../components/SignUp";
+import Articles from "../components/Articles";
+import Article from "../components/Article";
+import Categories from "../components/Categories";
+import Category from "../components/Category";
+import Author from "../components/Author";
+import Profile from "../components/Profile";
+import EditProfileForm from "../components/EditProfileForm";
+import Root from "../components/Root";
+
+import { Route } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import "./App.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    /* Wrap this Root Route to create Router here */
+    <Route path="/" element={<Root />}>
+      <Route path="about" element={<About />} />
+      <Route path="articles" element={<Articles />} />
+      <Route path="categories" element={<Categories />} />
+      <Route path="sign-up" element={<SignUp />} />
+      <Route path="profile" element={<Profile />} />
+    </Route>
+  )
+);
+
+function App() {
+  return (
+    <>
+      {/* Replace below and add Router Provider*/}
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
